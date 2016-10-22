cc4012c2ead06028a1e72d48918cd973062ac5f0
swechha <swechha@swechha.(none)>
2016-10-22 17:24:14 +0530

    third commit

--- Dictionaryapp/app/src/main/java/com/example/dictionaryapp/MainActivity.java
+++ Dictionaryapp/app/src/main/java/com/example/dictionaryapp/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.dictionaryapp;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -19,13 +24,24 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button searchButton;
     EditText wordInput;
+    TextView textView;
     private static final String  url0 = "http://api.wordnik.com:80/v4/word.json/";
     private static final String url1 = "/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=54d58ad1400c9bc39d59b4c5dae05829cb6d4086bcfe2c886";
     String url,response;
@@ -38,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         searchButton = (Button) findViewById(R.id.button);
         wordInput = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
 
         searchButton.setOnClickListener(this);
 
@@ -46,26 +63,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
-        url = url0 + wordInput.getText() + url1;
-        Log.i("url", url);
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(this, url, null, "application/json", new AsyncHttpResponseHandler() {
-
-            @Override
-            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody) {
-                String responsestring = new String(responseBody);
-                Toast toast = Toast.makeText(getApplicationContext(), "success" + statusCode, duration);
-                toast.show();
-                Log.i("response", responsestring);
-            }
-
-            @Override
-            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody, Throwable error) {
-                Toast toast = Toast.makeText(getApplicationContext(), "failed" + statusCode, duration);
-                toast.show();
-                Log.i("url", url);
-            }
-        });
+        ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+        dialog.setMessage("Loading....");
+        dialog.setCancelable(true);
+        dialog.show();
+        GetWordMeanTask getWordMeanTask = new GetWordMeanTask();
+        getWordMeanTask.execute(wordInput.getText().toString().trim().toLowerCase());
+        dialog.cancel();
+//        url = url0 + wordInput.getText() + url1;
+//        Log.i("url", url);
+//        AsyncHttpClient client = new AsyncHttpClient();
+//        client.get(this, url, null, "application/json", new AsyncHttpResponseHandler() {
+//
+//            @Override
+//            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody) {
+//                String responsestring = new String(responseBody);
+//                Toast toast = Toast.makeText(getApplicationContext(), "success" + statusCode, duration);
+//                toast.show();
+//                Log.i("response", responsestring);
+//            }
+//
+//            @Override
+//            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody, Throwable error) {
+//                Toast toast = Toast.makeText(getApplicationContext(), "failed" + statusCode, duration);
+//                toast.show();
+//                Log.i("url", url);
+//            }
+//        });
 
 //        DefaultHttpClient httpClient = new DefaultHttpClient();
 //        HttpEntity httpEntity = null;
@@ -81,21 +105,124 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //            e.printStackTrace();
 //        }
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Word Meaning");
-        builder.setMessage(wordInput.getText());
-        builder.setCancelable(true);
+    }
 
-        builder.setNegativeButton(
-                "Close",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
+    public class GetWordMeanTask extends AsyncTask<String, Void, String> {
+
+        private final String LOG_TAG = GetWordMeanTask.class.getSimpleName();
+
+        private String getWordMeaningFromJson(String JsonStr)
+                throws JSONException {
+
+            final String WORD_MEANING = "text";
+            String resultStr = "No word found.";
+            JSONArray jsonArray = new JSONArray(JsonStr);
+            if(jsonArray.length() > 0) {
+                JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+                resultStr = jsonObject.getString(WORD_MEANING);
+            }
+
+            Log.v(LOG_TAG, "Word meaning: " + resultStr);
+
+            return resultStr;
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String JsonStr = null;
+
+            try {
+
+                final String BASE_URL1 = "http://api.wordnik.com:80/v4/word.json/";
+                final String BASE_URL2 = "/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=54d58ad1400c9bc39d59b4c5dae05829cb6d4086bcfe2c886";
+                final String BASE_URL = BASE_URL1 + params[0] + BASE_URL2;
+
+                URL url = new URL(BASE_URL);
+
+                Log.v(LOG_TAG, "URi"+ BASE_URL);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                JsonStr = buffer.toString();
+
+                Log.v(LOG_TAG,"JSON String: "+ JsonStr);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
                     }
-                });
+                }
+            }
+            try {
+                return getWordMeaningFromJson(JsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
 
-        AlertDialog alert = builder.create();
-        alert.show();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String string) {
+            if(string != null){
+                textView.setText(wordInput.getText() + ": " + string);
+//                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+//                builder.setTitle("Word Meaning");
+//                builder.setMessage(wordInput.getText() + string);
+//                builder.setCancelable(true);
+//
+//                builder.setNegativeButton(
+//                        "Close",
+//                        new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog, int id) {
+//                                dialog.cancel();
+//                            }
+//                        });
+//
+//                AlertDialog alert = builder.create();
+//                alert.show();
+            }
+
+        }
     }
 
 }
