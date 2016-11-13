612237aa56e3679cd640754f885918fdededfb51
swechha <swechha@swechha.(none)>
2016-11-13 14:34:32 +0530

    forth commit

--- Dictionaryapp/app/src/main/java/com/example/dictionaryapp/SearchResultsActivity.java
+++ Dictionaryapp/app/src/main/java/com/example/dictionaryapp/SearchResultsActivity.java
@@ -1,38 +1,14 @@
 package com.example.dictionaryapp;
 
-import android.annotation.TargetApi;
-import android.app.ProgressDialog;
+import android.app.Activity;
 import android.app.SearchManager;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Build;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SearchView;
-import android.text.format.Time;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -41,68 +17,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.SimpleDateFormat;
+import static com.example.dictionaryapp.R.id.textView;
 
+/**
+ * Created by swechha on 11/12/16.
+ */
+public class SearchResultsActivity extends Activity {
 
-public class MainActivity extends AppCompatActivity {
-
-    TextView textView;
-    TextView resusltTxt;
-    SearchView searchView;
-    private static  final String  url0 = "http://api.wordnik.com:80/v4/word.json/";
-    private static final String url1 = "/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=54d58ad1400c9bc39d59b4c5dae05829cb6d4086bcfe2c886";
-    int duration = Toast.LENGTH_SHORT;
+    String query;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        textView = (TextView) findViewById(R.id.textView);
-        resusltTxt = (TextView) findViewById(R.id.meaning);
-
-        // Get the SearchView and set the searchable configuration
-        final SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        searchView = (SearchView) findViewById(R.id.searchView);
-        // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
-        //searchView.setOnSearchClickListener();
-        EditText txtSearch = ((EditText)searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text));
-        txtSearch.setHint("Search a word");
-        txtSearch.setHintTextColor(Color.LTGRAY);
-        txtSearch.setTextColor(Color.WHITE);
-        try {
-            Field mCursorDrawableRes = TextView.class.getDeclaredField("mCursorDrawableRes");
-            mCursorDrawableRes.setAccessible(true);
-            mCursorDrawableRes.set(txtSearch, 0);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                textView.setText(query);
-                GetWordMeanTask getWordMeanTask = new GetWordMeanTask();
-                getWordMeanTask.execute(query.trim().toLowerCase());
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
+    public void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.search_result);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        handleIntent(getIntent());
     }
 
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            query = intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(this, "Searching by: "+ query, Toast.LENGTH_SHORT).show();
+            GetWordMeanTask getWordMeanTask = new GetWordMeanTask();
+            getWordMeanTask.execute(query.trim().toLowerCase());
+        }
+    }
 
     public class GetWordMeanTask extends AsyncTask<String, Void, String> {
 
-        private final String LOG_TAG = GetWordMeanTask.class.getSimpleName();
+        private final String LOG_TAG = MainActivity.GetWordMeanTask.class.getSimpleName();
 
         private String getWordMeaningFromJson(String JsonStr)
                 throws JSONException {
@@ -197,7 +147,23 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String string) {
             if(string != null){
-                resusltTxt.setText(string);
+                System.out.println(string);
+                //textView.setText(query + ": " + string);
+//                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+//                builder.setTitle("Word Meaning");
+//                builder.setMessage(wordInput.getText() + string);
+//                builder.setCancelable(true);
+//
+//                builder.setNegativeButton(
+//                        "Close",
+//                        new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog, int id) {
+//                                dialog.cancel();
+//                            }
+//                        });
+//
+//                AlertDialog alert = builder.create();
+//                alert.show();
             }
 
         }
